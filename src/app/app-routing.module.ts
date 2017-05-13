3feba651bb78789574ba5f55fa49df204a5a5d28
DanPride <danpride@gmail.com>
2017-05-12 21:40:25 -0700

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
 const appRoutes: Routes = [
-{path: 'comments', loadChildren: './comments/comments.module#CommentsModule'},
-{path: 'drives', loadChildren: './drives/drives.module#DrivesModule'},
-{path: 'events', loadChildren: './events/events.module#EventsModule'},
-{path: 'outputs', loadChildren: './outputs/outputs.module#OutputsModule'},
-{path: 'people', loadChildren: './people/people.module#PeopleModule'},
-{path: 'places', loadChildren: './places/places.module#PlacesModule'},
-{path: 'rides', loadChildren: './rides/rides.module#RidesModule'},
-{path: 'runs', loadChildren: './runs/runs.module#RunsModule'}
+  {path: 'bugs', loadChildren: './bugs/bugs.module#BugsModule'},
+  {path: 'days', loadChildren: './days/days.module#DaysModule'},
+  {path: 'comments', loadChildren: './comments/comments.module#CommentsModule'},
+  {path: 'drives', loadChildren: './drives/drives.module#DrivesModule'},
+  {path: 'domains', loadChildren: './domains/domains.module#DomainsModule'},
+  {path: 'events', loadChildren: './events/events.module#EventsModule'},
+  {path: 'feeds', loadChildren: './feeds/feeds.module#FeedsModule'},
+  {path: 'iso639s', loadChildren: './iso639s/iso639s.module#Iso639sModule'},
+  {path: 'iso3166s', loadChildren: './iso3166s/iso3166s.module#Iso3166sModule'},
+  {path: 'invoices', loadChildren: './invoices/invoices.module#InvoicesModule'},
+  {path: 'langs', loadChildren: './langs/langs.module#LangsModule'},
+  {path: 'langs1', loadChildren: './langs1/langs1.module#Langs1Module'},
+  {path: 'langs2', loadChildren: './langs2/langs2.module#Langs2Module'},
+  {path: 'langs3', loadChildren: './langs3/langs3.module#Langs3Module'},
+  {path: 'langs4', loadChildren: './langs4/langs4.module#Langs4Module'},
+  {path: 'langerrs', loadChildren: './langerrs/langerrs.module#LangerrsModule'},
+  {path: 'letters', loadChildren: './letters/letters.module#LettersModule'},
+  {path: 'lettexts', loadChildren: './lettexts/lettexts.module#LettextsModule'},
+  {path: 'letvars', loadChildren: './letvars/letvars.module#LetvarsModule'},
+  {path: 'lineitems', loadChildren: './lineitems/lineitems.module#LineitemsModule'},
+  {path: 'logs', loadChildren: './logs/logs.module#LogsModule'},
+  {path: 'outputs', loadChildren: './outputs/outputs.module#OutputsModule'},
+  {path: 'people', loadChildren: './people/people.module#PeopleModule'},
+  {path: 'photos', loadChildren: './photos/photos.module#PhotosModule'},
+  {path: 'places', loadChildren: './places/places.module#PlacesModule'},
+  {path: 'popups', loadChildren: './popups/popups.module#PopupsModule'},
+  {path: 'posts', loadChildren: './posts/posts.module#PostsModule'},
+  {path: 'rides', loadChildren: './rides/rides.module#RidesModule'},
+  {path: 'runs', loadChildren: './runs/runs.module#RunsModule'},
+  {path: 'sched86s', loadChildren: './sched86s/sched86s.module#Sched86sModule'},
+  {path: 'schedules', loadChildren: './schedules/schedules.module#SchedulesModule'},
+  {path: 'users', loadChildren: './users/users.module#UsersModule'},
+  {path: 'zeros', loadChildren: './zeros/zeros.module#ZerosModule'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,68 +5,39 @@ import { HttpModule } from '@angular/http';
 import {AppComponent} from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {AppService} from './app.service';
-
+import { BugService } from './bugs/bugs.service';
 import { CommentService } from './comments/comments.service';
+import { DayService } from './days/days.service';
+import { DomainService } from './domains/domains.service';
 import { DriveService } from './drives/drives.service';
 import { EventService } from './events/events.service';
+import { FeedService } from './feeds/feeds.service';
+import { InvoiceService } from './invoices/invoices.service';
+import { Iso3166Service } from './iso3166s/iso3166s.service';
+import { Iso639Service } from './iso639s/iso639s.service';
+import { LangerrService } from './langerrs/langerrs.service';
+import { LangService } from './langs/langs.service';
+import { Lang1Service } from './langs1/langs1.service';
+import { Lang2Service } from './langs2/langs2.service';
+import { Lang3Service } from './langs3/langs3.service';
+import { Lang4Service } from './langs4/langs4.service';
+import { LetterService } from './letters/letters.service';
+import { LettextService } from './lettexts/lettexts.service';
+import { LetvarService } from './letvars/letvars.service';
+import { LineitemService } from './lineitems/lineitems.service';
+import { LogService } from './logs/logs.service';
 import { OutputService } from './outputs/outputs.service';
 import { PersonService } from './people/people.service';
+import { PhotoService } from './photos/photos.service';
 import { PlaceService } from './places/places.service';
+import { PopupService } from './popups/popups.service';
+import { PostService } from './posts/posts.service';
 import { RideService } from './rides/rides.service';
 import { RunService } from './runs/runs.service';
-
-/*
-import { LogsModule } from './logs/logs.module';
-import { BugsModule } from './bugs/bugs.module';
-
-//import { CommentsModule } from './comments/comments.module';
-import { DaysModule } from './days/days.module';
-//Moved domains to routing module for testing
-
-
-
-//import { EventsModule } from './events/events.module';
-import { FeedsModule } from './feeds/feeds.module';
-import { InvoicesModule } from './invoices/invoices.module';
-import { Iso3166Service } from './iso3166s/iso3166s.service';
-//import { Iso3166sModule } from './iso3166s/iso3166s.module';
-import { Iso639Service } from './iso639s/iso639s.service';
-//import { Iso639sModule } from './iso639s/iso639s.module';
-import { LangerrService } from './langerrs/langerrs.service';
-//import { LangerrsModule} from "./langerrs/langerrs.module";
-import { LangService } from './langs/langs.service';
-//import { LangsModule} from "./langs/langs.module";
-import { Lang1Service } from './langs1/langs1.service';
-//import { Langs1Module} from "./langs1/langs1.module";
-import { Lang2Service } from './langs2/langs2.service';
-//import { Langs2Module} from "./langs2/langs2.module";
-import { Lang3Service } from './langs3/langs3.service';
-//import { Langs3Module} from "./langs3/langs3.module";
-import { Lang4Service } from './langs4/langs4.service';
-//import { Langs4Module} from "./langs4/langs4.module";
-import { LettersModule } from './letters/letters.module';
-import { LettextsModule } from './lettexts/lettexts.module';
-import { LetvarsModule } from './letvars/letvars.module';
-import { LineitemsModule } from './lineitems/lineitems.module';
-
-//import { OutputsModule } from './outputs/outputs.module';
-//import { PeopleModule } from './people/people.module';
-
-import { PhotosModule } from './photos/photos.module';
-
-//import { PlacesModule } from './places/places.module';
-import { PopupsModule } from './popups/popups.module';
-import { PostsModule } from './posts/posts.module';
-
-//import { RidesModule } from './rides/rides.module';
-import { RunService } from './runs/runs.service';
-//import { RunsModule } from './runs/runs.module';
-import { Sched86sModule } from './sched86s/sched86s.module';
-import { SchedulesModule } from './schedules/schedules.module';
-import { UserService } from "./users/users.service";
-//import { UsersModule } from './users/users.module';
-import { ZerosModule } from './zeros/zeros.module';
- * */
+import { Sched86Service } from './sched86s/sched86s.service';
+import { ScheduleService } from './schedules/schedules.service';
+import { UserService } from './users/users.service';
+import { ZeroService } from './zeros/zeros.service';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
@@ -85,7 +56,43 @@ import 'rxjs/add/operator/toPromise';
   declarations: [
     AppComponent
   ],
-  providers: [AppService, CommentService, DriveService, EventService, OutputService, PersonService, PlaceService, RideService, RunService],
+  providers: [
+    AppService, BugService, CommentService, DriveService, EventService, OutputService, PersonService, PlaceService, RideService, RunService, 
+    UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/*
+//import { LogsModule } from './logs/logs.module';
+//import { BugsModule } from './bugs/bugs.module';
+//import { CommentsModule } from './comments/comments.module';
+//import { DaysModule } from './days/days.module';
+//Moved domains to routing module for testing
+//import { EventsModule } from './events/events.module';
+//import { FeedsModule } from './feeds/feeds.module';
+//import { InvoicesModule } from './invoices/invoices.module';
+//import { Iso3166sModule } from './iso3166s/iso3166s.module';
+//import { Iso639sModule } from './iso639s/iso639s.module';
+//import { LangerrsModule} from "./langerrs/langerrs.module";
+//import { LangsModule} from "./langs/langs.module";
+//import { Langs1Module} from "./langs1/langs1.module";
+//import { Langs2Module} from "./langs2/langs2.module";
+//import { Langs3Module} from "./langs3/langs3.module";
+//import { Langs4Module} from "./langs4/langs4.module";
+//import { LettersModule } from './letters/letters.module';
+//import { LettextsModule } from './lettexts/lettexts.module';
+//import { LetvarsModule } from './letvars/letvars.module';
+//import { LineitemsModule } from './lineitems/lineitems.module';
+//import { OutputsModule } from './outputs/outputs.module';
+//import { PeopleModule } from './people/people.module';
+//import { PhotosModule } from './photos/photos.module';
+//import { PlacesModule } from './places/places.module';
+//import { PopupsModule } from './popups/popups.module';
+//import { PostsModule } from './posts/posts.module';
+//import { RidesModule } from './rides/rides.module';
+//import { RunsModule } from './runs/runs.module';
+//import { Sched86sModule } from './sched86s/sched86s.module';
+//import { SchedulesModule } from './schedules/schedules.module';
+//import { UsersModule } from './users/users.module';
+//import { ZerosModule } from './zeros/zeros.module';
+ * */
